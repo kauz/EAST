0e262eadc736add7b1106927f2031ae059a1bf35
Artyom Popov <artyom.popov@bytebrand.net>
2019-10-02 15:49:49 +0300

    prepare runnable instance

--- eval.py
+++ eval.py
@@ -1,139 +1,124 @@
 import cv2
 import time
-import math
 import os
 import numpy as np
+import logging
+import functools
 import tensorflow as tf
-
-import locality_aware_nms as nms_locality
 import lanms
-
-tf.app.flags.DEFINE_string('test_data_path', '/tmp/ch4_test_images/images/', '')
-tf.app.flags.DEFINE_string('gpu_list', '0', '')
-tf.app.flags.DEFINE_string('checkpoint_path', '/tmp/east_icdar2015_resnet_v1_50_rbox/', '')
-tf.app.flags.DEFINE_string('output_dir', '/tmp/ch4_test_images/images/', '')
-tf.app.flags.DEFINE_bool('no_write_images', False, 'do not write images')
-
 import model
+from typing import Tuple
 from icdar import restore_rectangle
 
-FLAGS = tf.app.flags.FLAGS
-
-def get_images():
-    '''
-    find image files in test data path
-    :return: list of files found
-    '''
-    files = []
-    exts = ['jpg', 'png', 'jpeg', 'JPG']
-    for parent, dirnames, filenames in os.walk(FLAGS.test_data_path):
-        for filename in filenames:
-            for ext in exts:
-                if filename.endswith(ext):
-                    files.append(os.path.join(parent, filename))
-                    break
-    print('Find {} images'.format(len(files)))
-    return files
+logging.getLogger('tensorflow').setLevel(logging.CRITICAL)
 
 
-def resize_image(im, max_side_len=2400):
-    '''
-    resize image to a size multiple of 32 which is required by the network
-    :param im: the resized image
-    :param max_side_len: limit of max image size to avoid out of memory in gpu
-    :return: the resized image and the resize ratio
-    '''
-    h, w, _ = im.shape
+class ImageProcessor:
+    def __init__(self, argv):
+        tesseract_config = {"l": argv["language"], "oem": argv["oem"], "psm": argv["psm"]}
+        patterns = argv["patterns"]
+        if patterns:
+            tesseract_config["patterns"] = patterns
 
-    resize_w = w
-    resize_h = h
+        self.verbose: int = argv["verbose"]
+        self.checkpoint_path: str = argv['checkpoint_path']
+        self.min_confidence: float = argv['min_confidence']
+        self.padding: Tuple[float, float] = (argv['padding_horizontal'], argv['padding_vertical'])
+        self.preview_mode: int = argv["preview_mode"]
+        self.format: str = argv["format"]
+        os.environ['CUDA_VISIBLE_DEVICES'] = argv["gpu_list"]
 
-    # limit the max side
-    if max(resize_h, resize_w) > max_side_len:
-        ratio = float(max_side_len) / resize_h if resize_h > resize_w else float(max_side_len) / resize_w
-    else:
-        ratio = 1.
-    resize_h = int(resize_h * ratio)
-    resize_w = int(resize_w * ratio)
+    @staticmethod
+    def _resize_image(im, max_side_len=2400):
+        """
+        resize image to a size multiple of 32 which is required by the network
+        :param im: the resized image
+        :param max_side_len: limit of max image size to avoid out of memory in gpu
+        :return: the resized image and the resize ratio
+        """
+        h, w, _ = im.shape
 
-    resize_h = resize_h if resize_h % 32 == 0 else (resize_h // 32 - 1) * 32
-    resize_w = resize_w if resize_w % 32 == 0 else (resize_w // 32 - 1) * 32
-    resize_h = max(32, resize_h)
-    resize_w = max(32, resize_w)
-    im = cv2.resize(im, (int(resize_w), int(resize_h)))
+        resize_w = w
+        resize_h = h
 
-    ratio_h = resize_h / float(h)
-    ratio_w = resize_w / float(w)
+        # limit the max side
+        if max(resize_h, resize_w) > max_side_len:
+            ratio = float(max_side_len) / resize_h if resize_h > resize_w else float(max_side_len) / resize_w
+        else:
+            ratio = 1.
+        resize_h = int(resize_h * ratio)
+        resize_w = int(resize_w * ratio)
 
-    return im, (ratio_h, ratio_w)
+        resize_h = resize_h if resize_h % 32 == 0 else (resize_h // 32 - 1) * 32
+        resize_w = resize_w if resize_w % 32 == 0 else (resize_w // 32 - 1) * 32
+        resize_h = max(32, resize_h)
+        resize_w = max(32, resize_w)
+        im = cv2.resize(im, (int(resize_w), int(resize_h)))
 
+        ratio_h = resize_h / float(h)
+        ratio_w = resize_w / float(w)
 
-def detect(score_map, geo_map, timer, score_map_thresh=0.8, box_thresh=0.1, nms_thres=0.2):
-    '''
-    restore text boxes from score map and geo map
-    :param score_map:
-    :param geo_map:
-    :param timer:
-    :param score_map_thresh: threshhold for score map
-    :param box_thresh: threshhold for boxes
-    :param nms_thres: threshold for nms
-    :return:
-    '''
-    if len(score_map.shape) == 4:
-        score_map = score_map[0, :, :, 0]
-        geo_map = geo_map[0, :, :, ]
-    # filter the score map
-    xy_text = np.argwhere(score_map > score_map_thresh)
-    # sort the text boxes via the y axis
-    xy_text = xy_text[np.argsort(xy_text[:, 0])]
-    # restore
-    start = time.time()
-    text_box_restored = restore_rectangle(xy_text[:, ::-1]*4, geo_map[xy_text[:, 0], xy_text[:, 1], :]) # N*4*2
-    print('{} text boxes before nms'.format(text_box_restored.shape[0]))
-    boxes = np.zeros((text_box_restored.shape[0], 9), dtype=np.float32)
-    boxes[:, :8] = text_box_restored.reshape((-1, 8))
-    boxes[:, 8] = score_map[xy_text[:, 0], xy_text[:, 1]]
-    timer['restore'] = time.time() - start
-    # nms part
-    start = time.time()
-    # boxes = nms_locality.nms_locality(boxes.astype(np.float64), nms_thres)
-    boxes = lanms.merge_quadrangle_n9(boxes.astype('float32'), nms_thres)
-    timer['nms'] = time.time() - start
+        return im, (ratio_h, ratio_w)
 
-    if boxes.shape[0] == 0:
-        return None, timer
+    @staticmethod
+    def _detect(score_map, geo_map, timer, score_map_thresh=0.8, box_thresh=0.1, nms_thres=0.2):
+        """
+        restore text boxes from score map and geo map
+        :param score_map:
+        :param geo_map:
+        :param timer:
+        :param score_map_thresh: threshhold for score map
+        :param box_thresh: threshhold for boxes
+        :param nms_thres: threshold for nms
+        :return:
+        """
+        if len(score_map.shape) == 4:
+            score_map = score_map[0, :, :, 0]
+            geo_map = geo_map[0, :, :, ]
+        # filter the score map
+        xy_text = np.argwhere(score_map > score_map_thresh)
+        # sort the text boxes via the y axis
+        xy_text = xy_text[np.argsort(xy_text[:, 0])]
+        # restore
+        start = time.time()
+        text_box_restored = restore_rectangle(xy_text[:, ::-1] * 4, geo_map[xy_text[:, 0], xy_text[:, 1], :])  # N*4*2
+        print('{} text boxes before nms'.format(text_box_restored.shape[0]))
+        boxes = np.zeros((text_box_restored.shape[0], 9), dtype=np.float32)
+        boxes[:, :8] = text_box_restored.reshape((-1, 8))
+        boxes[:, 8] = score_map[xy_text[:, 0], xy_text[:, 1]]
+        timer['restore'] = time.time() - start
+        # nms part
+        start = time.time()
+        # boxes = nms_locality.nms_locality(boxes.astype(np.float64), nms_thres)
+        boxes = lanms.merge_quadrangle_n9(boxes.astype('float32'), nms_thres)
+        timer['nms'] = time.time() - start
 
-    # here we filter some low score boxes by the average score map, this is different from the orginal paper
-    for i, box in enumerate(boxes):
-        mask = np.zeros_like(score_map, dtype=np.uint8)
-        cv2.fillPoly(mask, box[:8].reshape((-1, 4, 2)).astype(np.int32) // 4, 1)
-        boxes[i, 8] = cv2.mean(score_map, mask)[0]
-    boxes = boxes[boxes[:, 8] > box_thresh]
+        if boxes.shape[0] == 0:
+            return None, timer
 
-    return boxes, timer
+        # here we filter some low score boxes by the average score map, this is different from the orginal paper
+        for i, box in enumerate(boxes):
+            mask = np.zeros_like(score_map, dtype=np.uint8)
+            cv2.fillPoly(mask, box[:8].reshape((-1, 4, 2)).astype(np.int32) // 4, 1)
+            boxes[i, 8] = cv2.mean(score_map, mask)[0]
+        boxes = boxes[boxes[:, 8] > box_thresh]
 
+        return boxes, timer
 
-def sort_poly(p):
-    min_axis = np.argmin(np.sum(p, axis=1))
-    p = p[[min_axis, (min_axis+1)%4, (min_axis+2)%4, (min_axis+3)%4]]
-    if abs(p[0, 0] - p[1, 0]) > abs(p[0, 1] - p[1, 1]):
-        return p
-    else:
-        return p[[0, 3, 2, 1]]
+    @staticmethod
+    def _sort_poly(p):
+        min_axis = np.argmin(np.sum(p, axis=1))
+        p = p[[min_axis, (min_axis + 1) % 4, (min_axis + 2) % 4, (min_axis + 3) % 4]]
+        if abs(p[0, 0] - p[1, 0]) > abs(p[0, 1] - p[1, 1]):
+            return p
+        else:
+            return p[[0, 3, 2, 1]]
 
+    def text_detection(self, image: str):
+        return self.get_predictor()(image)
 
-def main(argv=None):
-    import os
-    os.environ['CUDA_VISIBLE_DEVICES'] = FLAGS.gpu_list
-
-
-    try:
-        os.makedirs(FLAGS.output_dir)
-    except OSError as e:
-        if e.errno != 17:
-            raise
-
-    with tf.get_default_graph().as_default():
+    @functools.lru_cache(maxsize=100)
+    def get_predictor(self):
         input_images = tf.placeholder(tf.float32, shape=[None, None, None, 3], name='input_images')
         global_step = tf.get_variable('global_step', [], initializer=tf.constant_initializer(0), trainable=False)
 
@@ -142,55 +127,49 @@ def main(argv=None):
         variable_averages = tf.train.ExponentialMovingAverage(0.997, global_step)
         saver = tf.train.Saver(variable_averages.variables_to_restore())
 
-        with tf.Session(config=tf.ConfigProto(allow_soft_placement=True)) as sess:
-            ckpt_state = tf.train.get_checkpoint_state(FLAGS.checkpoint_path)
-            model_path = os.path.join(FLAGS.checkpoint_path, os.path.basename(ckpt_state.model_checkpoint_path))
-            print('Restore from {}'.format(model_path))
-            saver.restore(sess, model_path)
+        sess = tf.Session(config=tf.ConfigProto(allow_soft_placement=True))
 
-            im_fn_list = get_images()
-            for im_fn in im_fn_list:
-                im = cv2.imread(im_fn)[:, :, ::-1]
-                start_time = time.time()
-                im_resized, (ratio_h, ratio_w) = resize_image(im)
+        ckpt_state = tf.train.get_checkpoint_state(self.checkpoint_path)
+        model_path = os.path.join(self.checkpoint_path, os.path.basename(ckpt_state.model_checkpoint_path))
+        saver.restore(sess, model_path)
 
-                timer = {'net': 0, 'restore': 0, 'nms': 0}
-                start = time.time()
-                score, geometry = sess.run([f_score, f_geometry], feed_dict={input_images: [im_resized]})
-                timer['net'] = time.time() - start
+        def predictor(img):
+            start_time = time.time()
 
-                boxes, timer = detect(score_map=score, geo_map=geometry, timer=timer)
-                print('{} : net {:.0f}ms, restore {:.0f}ms, nms {:.0f}ms'.format(
-                    im_fn, timer['net']*1000, timer['restore']*1000, timer['nms']*1000))
+            # rtparams['image_size'] = '{}x{}'.format(img.shape[1], img.shape[0])
+            im = cv2.imread(img)[:, :, ::-1]
+            im_resized, (ratio_h, ratio_w) = self._resize_image(im)
 
-                if boxes is not None:
-                    boxes = boxes[:, :8].reshape((-1, 4, 2))
-                    boxes[:, :, 0] /= ratio_w
-                    boxes[:, :, 1] /= ratio_h
+            timer = {'net': 0, 'restore': 0, 'nms': 0}
+            start = time.time()
+            score, geometry = sess.run([f_score, f_geometry], feed_dict={input_images: [im_resized]})
+            timer['net'] = time.time() - start
 
-                duration = time.time() - start_time
-                print('[timing] {}'.format(duration))
+            boxes, timer = self._detect(score_map=score, geo_map=geometry, timer=timer)
+            print('{} : net {:.0f}ms, restore {:.0f}ms, nms {:.0f}ms'.format(
+                img, timer['net'] * 1000, timer['restore'] * 1000, timer['nms'] * 1000))
 
-                # save to file
-                if boxes is not None:
-                    res_file = os.path.join(
-                        FLAGS.output_dir,
-                        '{}.txt'.format(
-                            os.path.basename(im_fn).split('.')[0]))
+            if boxes is not None:
+                boxes = boxes[:, :8].reshape((-1, 4, 2))
+                boxes[:, :, 0] /= ratio_w
+                boxes[:, :, 1] /= ratio_h
 
-                    with open(res_file, 'w') as f:
-                        for box in boxes:
-                            # to avoid submitting errors
-                            box = sort_poly(box.astype(np.int32))
-                            if np.linalg.norm(box[0] - box[1]) < 5 or np.linalg.norm(box[3]-box[0]) < 5:
-                                continue
-                            f.write('{},{},{},{},{},{},{},{}\r\n'.format(
-                                box[0, 0], box[0, 1], box[1, 0], box[1, 1], box[2, 0], box[2, 1], box[3, 0], box[3, 1],
-                            ))
-                            cv2.polylines(im[:, :, ::-1], [box.astype(np.int32).reshape((-1, 1, 2))], True, color=(255, 255, 0), thickness=1)
-                if not FLAGS.no_write_images:
-                    img_path = os.path.join(FLAGS.output_dir, os.path.basename(im_fn))
-                    cv2.imwrite(img_path, im[:, :, ::-1])
+            duration = time.time() - start_time
+            print('[timing] {}'.format(duration))
 
-if __name__ == '__main__':
-    tf.app.run()
+            # save to file
+            if boxes is not None:
+                for box in boxes:
+                    # to avoid submitting errors
+                    box = self._sort_poly(box.astype(np.int32))
+                    if np.linalg.norm(box[0] - box[1]) < 5 or np.linalg.norm(box[3] - box[0]) < 5:
+                        continue
+                    print(box)
+                    cv2.polylines(im[:, :, ::-1], [box.astype(np.int32).reshape((-1, 1, 2))], True,
+                                  color=(0, 255, 0), thickness=2)
+
+            title = os.path.basename(img)
+            cv2.imshow(title, im[:, :, ::-1])
+            cv2.waitKey(0)
+
+        return predictor
